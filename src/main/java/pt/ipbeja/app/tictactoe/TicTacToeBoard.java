93d653d59d0bb7a195adafb5bbe7d38fa25743f1
Goodman <neyvaldospencergomes@gmail.com>
2025-03-03 17:22:38 +0000

    build: Atualização do metodo button handler

--- src/main/java/pt/ipbeja/app/tictactoe/TicTacToeBoard.java
+++ src/main/java/pt/ipbeja/app/tictactoe/TicTacToeBoard.java
@@ -8,50 +8,79 @@ import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
 
 import javax.lang.model.util.ElementScanner6;
+import java.nio.file.Paths;
 
 public class TicTacToeBoard extends GridPane {
 
-    static public final int SIZE = 3;
-    //static boolean isX = true;
 
-    public TicTacToeBoard()
+     public final int SIZE = 3;     // Size fix grade 3x3
+
+    static boolean isTurnX = true;
+
+     /* Image of Player X  */
+     static String urlX = Paths.get("/Users/neyvaldo/IdeaProjects/GameOldTicTacToe/src/main/resources/src/player1.png").toUri().toString();
+     static Image imageX = new Image(urlX);
+
+    /* Image of Player O  */
+    static String urlO = Paths.get("/Users/neyvaldo/IdeaProjects/GameOldTicTacToe/src/main/resources/src/player2.png").toUri().toString();
+    static Image imageO = new Image(urlO);
+
+    /* Image Empty */
+    static String urlEmpy = Paths.get("/Users/neyvaldo/IdeaProjects/GameOldTicTacToe/src/main/resources/src/noplayer.png").toUri().toString();
+    static Image imageEmpty = new Image(urlEmpy);
+    //static ImageView viewEmpty= new ImageView(imageEmpty);
+
+
+
+    public TicTacToeBoard()  // Constructor
     {
         this.createBoard();
-
     }
 
+    /*
+    * Create table of game tic-tac-toe with in grade of button
+    * */
     private void createBoard ()
     {
-        ImageView view  = new ImageView(new Image("/resources/noplaze.png"));
         ButtonHandler bHandler = new ButtonHandler();
         for (int line = 0; line < SIZE ; line++)
         {
             for (int col = 0; col < SIZE; col++)
             {
-                Button btt = new Button();
-                btt.setPrefSize(100,100);
-                btt.setGraphic(view);
-                btt.setOnAction(bHandler);
-                this.add(btt, col, line);
+                TicTacToeButton ticTacToeButton = new TicTacToeButton();
+                ticTacToeButton.setPrefSize(200,200); // Size of button
+                //btt.setGraphic(view);
+                ticTacToeButton.setOnAction(bHandler);
+                this.add(ticTacToeButton, col, line); // Add button in grade
             }
         }
 
     }
+
+    /*
+    * Button que despara ação quando é clicado
+    * */
      static class ButtonHandler implements EventHandler<ActionEvent> {
 
          @Override
          public void handle(ActionEvent actionEvent)
          {
-             Button getButtonClick = (Button) (actionEvent.getSource());
-             String buttonClick = getButtonClick.getText();
+             TicTacToeButton getButtonClick = (TicTacToeButton) (actionEvent.getSource());
+             //String buttonClick = getButtonClick.getText();
 
-             if (buttonClick.equals("X"))
-             {
-                 getButtonClick.setText("O");
 
-             } else {
-                 getButtonClick.setText("X");
+             if( getButtonClick.getGraphic() == null) {
+                 if (isTurnX) {
+                      ImageView viewX = new ImageView(imageX);
+                      getButtonClick.setGraphic(viewX);
 
+                 } else {
+
+                     ImageView viewO = new ImageView(imageO);
+                     getButtonClick.setGraphic(viewO);
+                 }
+                 
+                 isTurnX = !isTurnX;
              }
          }
      }
---
+++ src/main/java/pt/ipbeja/app/tictactoe/TicTacToeButton.java
@@ -0,0 +1,9 @@
+package pt.ipbeja.app.tictactoe;
+
+import javafx.scene.control.Button;
+
+public class TicTacToeButton extends Button {
+
+
+
+}
