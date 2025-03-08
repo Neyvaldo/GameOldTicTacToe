db325757b1950b4ddea67d5d3ce3a6f4332912eb
Goodman <neyvaldospencergomes@gmail.com>
2025-03-08 13:20:49 +0000

    Update:Implementaco da classe TicTacToeButton com metodos...

--- src/main/java/pt/ipbeja/app/tictactoe/TicTacToeBoard.java
+++ src/main/java/pt/ipbeja/app/tictactoe/TicTacToeBoard.java
@@ -1,36 +1,21 @@
 package pt.ipbeja.app.tictactoe;
 
+import javafx.application.Platform;
 import javafx.css.Size;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Node;
-import javafx.scene.control.Alert;
-import javafx.scene.control.Button;
+import javafx.scene.control.*;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
-
-import java.nio.file.Paths;
+import javafx.scene.layout.VBox;
 
 public class TicTacToeBoard extends GridPane {
 
-
-     public static final int SIZE = 3;     // Size fix grade 3x3
+    public static final int SIZE = 3;     // Size fix grade 3x3
 
     static boolean isTurnX = true;
-
-     /* Image of Player X  */
-     static String urlX = Paths.get("/Users/neyvaldo/IdeaProjects/GameOldTicTacToe/src/main/resources/src/player1.png").toUri().toString();
-     static Image imageX = new Image(urlX);
-
-    /* Image of Player O  */
-    static String urlO = Paths.get("/Users/neyvaldo/IdeaProjects/GameOldTicTacToe/src/main/resources/src/player2.png").toUri().toString();
-    static Image imageO = new Image(urlO);
-
-    /* Image Empty */
-    static String urlEmpy = Paths.get("/Users/neyvaldo/IdeaProjects/GameOldTicTacToe/src/main/resources/src/noplayer.png").toUri().toString();
-    static Image imageEmpty = new Image(urlEmpy);
-
 
     static int count = 0;
 
@@ -40,77 +25,100 @@ public class TicTacToeBoard extends GridPane {
     }
 
     /*
-    * Create table of game tic-tac-toe with in grade of button
-    * */
-    private void createBoard ()
-    {
+     * Create table of game tic-tac-toe with in grade of button
+     * */
+    private void createBoard() {
         getChildren().clear();
         ButtonHandler bHandler = new ButtonHandler();
-        for (int line = 0; line < SIZE ; line++)
-        {
-            for (int col = 0; col < SIZE; col++)
-            {
+        for (int line = 0; line < SIZE; line++) {
+            for (int col = 0; col < SIZE; col++) {
                 TicTacToeButton ticTacToeButton = new TicTacToeButton();
-                ticTacToeButton.setPrefSize(200,200); // Size of button
                 ticTacToeButton.setOnAction(bHandler);
-                ImageView viewEmpty= new ImageView(imageEmpty);
-                ticTacToeButton.setGraphic(viewEmpty);
                 this.add(ticTacToeButton, col, line); // Add button in grade
             }
         }
     }
+
     /**
      * Faz o Reset o tabuleiro do jogo Tic-Tac-Toe para seu estado inicial.
-     * Reativa todos os botões, redefine as suas imagens para Empty,
-     * zera o contador de cliques e restaura o turno inicial.
+     * Limpa os child de GridPane, redefine o contador para 0
+     *  restaura o turno para `X` e recria o tabuleiro.
      */
-    public void resetGame()
-    {
+    public void resetGame() {
+        getChildren().clear();
         count = 0;
-        for (Node child : getChildren()) {
-            Button btn = (Button) child;
-            ImageView imageView = (ImageView) btn.getGraphic();
+        isTurnX = true;
+        createBoard();
+    }
 
-            imageView.setImage(imageEmpty);
-            btn.setDisable(false);
+    /*
+     * Button que, despara ação quando é clicado
+     * Alterna entre os jogadores "X" e "O", incrementa o contador de cliques,
+     * desativa botões clicados, e exibe a mensagem de fim de jogo quando todos são usados.
+     * */
+
+    class ButtonHandler implements EventHandler<ActionEvent> {
+        @Override
+        public void handle(ActionEvent actionEvent) {
+            TicTacToeButton getButtonClick = (TicTacToeButton) (actionEvent.getSource());
+            count++;
+
+            if (!getButtonClick.isCancelButton()) {
+                if (isTurnX == true) {
+                    getButtonClick.setPlayerX();
+                } else {
+                    getButtonClick.setPlayerO();
+                }
+                // OU
+                //getButtonClick.setGraphic(isTurnX ? new ImageView(TicTacToeButton.imageX): new ImageView(TicTacToeButton.imageO));
+
+                isTurnX = !isTurnX;
+
+                getButtonClick.setDisable(true);
+
+            }
+
+            if (count == SIZE * SIZE) // Verifica se todos os botoes foram clicados
+            {
+                showOverGameAlert();
+                resetGame();
+            }
+        }
+
+        /**
+         * Exibe a menssagem de fim de jogo usando Alert com DialogPane
+         * Inclui um Label com a mensagem "O jogo terminou" e um botão "Reset" para reiniciar o jogo.
+         * O botao OK, encerra o Aplicativo
+         */
+        private void showOverGameAlert() {
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("END GAME");
+
+            DialogPane dialogPane = alert.getDialogPane();
+
+            Button resetBtt = new Button("Reset");
+            resetBtt.setOnAction(actionEvent -> {
+                resetGame();
+                alert.close();
+            });
+
+
+            Label label = new Label("O jogo terminou");
+            VBox alertLayout = new VBox();
+            alertLayout.getChildren().addAll(label, resetBtt);
+            alertLayout.setSpacing(15);
+
+            dialogPane.setContent(alertLayout);
+
+
+            alert.showAndWait().ifPresent(buttonType ->
+            {
+                if (buttonType==ButtonType.OK)
+                {
+                    Platform.exit();
+                }
+            });
 
         }
     }
-    /*
-    * Button que, despara ação quando é clicado
-    * */
-      class ButtonHandler implements EventHandler<ActionEvent> {
-
-        @Override
-         public void handle(ActionEvent actionEvent)
-         {
-             TicTacToeButton getButtonClick = (TicTacToeButton) (actionEvent.getSource());
-             count++;
-             //String buttonClick = getButtonClick.getText();
-             ImageView imageView = (ImageView) getButtonClick.getGraphic();
-             if( !getButtonClick.isCancelButton()) {
-                if (isTurnX) {
-                    //ImageView viewX = new ImageView(imageX);
-                    //getButtonClick.setGraphic(viewX);
-                    imageView.setImage(imageX);
-
-                } else {
-                    //ImageView viewO = new ImageView(imageO);
-                    //getButtonClick.setGraphic(viewO);
-                    imageView.setImage(imageO);
-                }
-                isTurnX = !isTurnX;
-                getButtonClick.setDisable(true); // Desativa o botão quando ele é clicado
-            }
-
-            if(count == (SIZE * SIZE)) // Verifica se todos os botoes foram clicados
-            {
-                Alert alert = new Alert(Alert.AlertType.INFORMATION, "Over Game");
-                alert.showAndWait();// Alerta a informar o termino do jogo
-
-                resetGame();
-            }
-
-         }
-     }
 }
--- src/main/java/pt/ipbeja/app/tictactoe/TicTacToeButton.java
+++ src/main/java/pt/ipbeja/app/tictactoe/TicTacToeButton.java
@@ -1,10 +1,50 @@
 package pt.ipbeja.app.tictactoe;
 
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
-public class TicTacToeButton extends Button {
+import java.nio.file.Paths;
 
+public class TicTacToeButton extends Button
+{
 
+    /* Image of Player X  */
+    public static String urlX = Paths.get("/Users/neyvaldo/IdeaProjects/GameOldTicTacToe/src/main/resources/src/player1.png").toUri().toString();
+    static Image imageX = new Image(urlX);
 
+    /* Image of Player O  */
+    public static String urlO = Paths.get("/Users/neyvaldo/IdeaProjects/GameOldTicTacToe/src/main/resources/src/player2.png").toUri().toString();
+    public static Image imageO = new Image(urlO);
+
+    /* Image Empty */
+    public static String urlEmpy = Paths.get("/Users/neyvaldo/IdeaProjects/GameOldTicTacToe/src/main/resources/src/noplayer.png").toUri().toString();
+    public  Image imageEmpty = new Image(urlEmpy);
+
+    private final ImageView imageView;
+
+    public TicTacToeButton() {
+        this.imageView = new ImageView(imageEmpty);
+        this.setGraphic(imageView);
+        this.setPrefSize(150,150); // Size of button
+    }
+
+    /**
+     * Define a imagem do botão como a do jogador "X".
+     * Atualiza o ImageView associado ao botão para exibir a imagem de "X".
+     */
+    public void setPlayerX()
+    {
+        this.imageView.setImage(imageX);
+    }
+
+    /**
+     * Define a imagem do botão como a do jogador "O".
+     * Atualiza o ImageView associado ao botão para exibir a imagem de "O".
+     */
+    public void setPlayerO()
+    {
+        this.imageView.setImage(imageO);
+    }
 
 }
